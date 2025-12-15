02e435e8b9d9115685147f860e5bc1665fcea167
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2025-12-15 20:06:33 +0200

    exercitiul4

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Moldovan_Paula_Lab1.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("hello")]
+        public string Hello() => "Salut!";
+
+        [HttpGet("hello/{name}")]
+        public string HelloByName(string name) => $"Salut, {name}!";
+
+        [HttpGet("calc/{a:int}/{b:int}")]
+        public string Calculate(int a, int b) => $"Suma este: {a + b}";
+    }
+}
