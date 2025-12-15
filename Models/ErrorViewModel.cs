d64b13f68807302fe8a4bb8749ed603f86602085
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2025-12-15 17:59:08 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Moldovan_Paula_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
