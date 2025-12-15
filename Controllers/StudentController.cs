0f88df46bb2cd34393176ec95ab8f4a68fab011c
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2025-12-15 19:31:42 +0200

    exercitiul1

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moldovan_Paula_Lab1.Models;
+
+namespace Moldovan_Paula_Lab1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            var student = new Student { Name = "Ana", Age = 21 };
+            return View(student);
+        }
+        
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Moldovan_Paula_Lab1.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
