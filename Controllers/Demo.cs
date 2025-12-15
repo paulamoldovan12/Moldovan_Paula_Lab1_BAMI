05fb7868aa4d4d82ea40994de519d46c2d907540
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2025-12-15 18:37:45 +0200

    ex 28

---
+++ Controllers/Demo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Moldovan_Paula_Lab1.Controllers
+{
+    public class Demo : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit!";
+        }
+
+        public string Salut()
+        {
+            return "Salut!";
+        }
+        public string MesajPersonalizat(String nume, int numar)
+        {
+            nume = "Paula";
+            numar = 2;
+            return "Bine ai venit " + nume + " pentru a " + numar + "-a oara";
+        }
+    }
+}
